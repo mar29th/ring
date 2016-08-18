16ee98e442bfc5a05f3cbbd3a885d79d3e80851e
lafickens <yuxiaohang87878@msn.com>
2016-08-18 21:15:38 +0800

    Initial release

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
 
-setup(name='ring',
-      version='0.1',
-      url='http://github.com/mar29th/ring',
-      author='Douban Platform CoreSys Team',
-      author_email='platform@douban.com',
-      license='BSD',
-      packages=['ring'],
-      zip_safe=False)
+setup(
+    name='ringmq',
+    version='0.1',
+    url='http://github.com/mar29th/ring',
+    author='Douban Inc.',
+    author_email='platform@douban.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Networking',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    packages=['ring', 'ring.tests', 'ring.benchmark']
+)
